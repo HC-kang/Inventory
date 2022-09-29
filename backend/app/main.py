e121bea074434f8d9cdccbdf067e7136fba04785
Heechan Kang <weston0713@gmail.com>
2022-09-30 07:49:02 +0900

    220930 handlers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 import time
 from pathlib import Path
 
-from fastapi import FastAPI, APIRouter, Request, Depends
+from fastapi import FastAPI, APIRouter, Request, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,6 +9,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.api_v1.api import api_router
 from app.api.routes.api_v1.api import router as api_router2
 from app.core.config import get_app_settings
+from app.api.errors.http_error import http_error_handler
+from app.api.errors.validation_error import http422_error_handler
+from app.api.errors.validation_error import RequestValidationError
+from app.core.events import create_start_app_handler, create_stop_app_handler
 
 
 def get_application() -> FastAPI:
@@ -34,6 +38,18 @@ def get_application() -> FastAPI:
     #     process_time = time.time() - start_time
     #     response.headers["X-Process-Time"] = str(process_time)
     #     return response
+    
+    application.add_event_handler(
+        "startup",
+        create_start_app_handler(application, settings),
+    )
+    application.add_event_handler(
+        "shutdown",
+        create_stop_app_handler(application),
+    )
+    
+    application.add_exception_handler(HTTPException, http_error_handler)
+    application.add_exception_handler(RequestValidationError, http422_error_handler)
 
     # application.include_router(api_router, prefix=settings.api_prefix)
     application.include_router(api_router2, prefix=settings.api_prefix)
