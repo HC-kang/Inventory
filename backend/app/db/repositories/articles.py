8d9341ec4dcc994c056605b2c2b56fb57c032c19
Heechan Kang <weston0713@gmail.com>
2022-10-05 07:50:58 +0900

    221005 articles ongoing

--- backend/app/db/repositories/articles.py
+++ backend/app/db/repositories/articles.py
@@ -1,11 +1,20 @@
-from typing import Optional, Sequence
+from typing import Optional, Sequence, List, Union
 
 from asyncpg import Connection
+from pypika import Query
 
 from app.models.domain.users import User
 from app.models.domain.articles import Ar, Article
 from app.db.repositories.base import BaseRepository
 from app.db.queries.queries import queries
+from app.db.queries.tables import (
+    Parameter,
+    articles,
+    articles_to_tags,
+    favorites,
+    tags as tags_table,
+    users,
+)
 
 
 AUTHOR_USERNAME_ALIAS = "author_username"
@@ -49,3 +58,126 @@ class ArticlesRepository(BaseRepository):
             author_username=article_row[AUTHOR_USERNAME_ALIAS],
             requested_user=author,
         )
+
+    async def update_article(
+        self,
+        *,
+        article: Article,
+        slug: Optional[str] = None,
+        title: Optional[str] = None,
+        body: Optional[str] = None,
+        description: Optional[str] = None,
+    ) -> Article:
+        updated_article = article.copy(deep=True)
+        updated_article.slug = slug or updated_article.slug
+        updated_article.title = title or article.title
+        updated_article.body = body or article.body
+        updated_article.description = description or article.description
+        
+        async with self.connection.transaction():
+            updated_article.updated_at = await queries.update_article(
+                self.connection,
+                slug=article.slug,
+                author_username= article.author.username,
+                new_slug=updated_article.slug,
+                new_title=updated_article.title,
+                new_body=updated_article.body,
+                new_description=updated_article.description,
+            )
+        
+        return updated_article
+    
+    async def delete_article(self, *, article: Article) -> None:
+        async with self.connection.transaction():
+            await queries.delete_article(
+                self.connection,
+                slug=article.slug,
+                author_username=article.author.username,
+            )
+    
+    async def filter_articles(
+        self,
+        *,
+        tag: Optional[str] = None,
+        author: Optional[str] = None,
+        favorited: Optional[str] = None,
+        limit: int = 20,
+        offset: int = 0,
+        requested_user: Optional[User] = None,
+    ) -> List[Article]:
+        query_params: List[Union[str, int]] = []
+        query_params_count = 0
+        
+        query = Query.from_(
+            articles,
+        ).select(
+            articles.id,
+            articles.slug,
+            articles.title,
+            articles.description,
+            articles.body,
+            articles.created_at,
+            articles.updated_at,
+            Query.from_(
+                users,
+            ).where(
+                users.id == articles.author_id,
+            ).select(
+                users.username,
+            ).as_(
+                AUTHOR_USERNAME_ALIAS,
+            )
+        )
+
+        if tag:
+            query_params.append(tag)
+            query_params_count += 1
+            
+            query = query.join(
+                articles_to_tags,
+            ).on(
+                (articles.id == articles_to_tags.article_id) & (
+                    articles_to_tags.tag == Query.from_(
+                        tags_table,
+                    ).where(
+                        tags_table,tag == Parameter(query_params_count),
+                    ).select(
+                        tags_table.tag,
+                    )
+                )
+            )
+        
+        if author:
+            query_params.append(author)
+            query_params_count += 1
+            
+            query = query.join(
+                users,
+            ).on(
+                (articles.id == favorites.article_id) & (
+                    favorites.user_id == Query.from_(
+                        users,
+                    ).where(
+                        users.username == Parameter(query_params_count),
+                    ).select(
+                        users.id,
+                    )
+                )
+            )
+
+        query = query.limit(Parameter(query_params_count + 1)).offset(
+            Parameter(query_params_count + 2),
+        )
+        query_params.extends([limit, offset])
+        
+        articles_rows = await self.connection.fetch(query.get_sql(), *query_params)
+
+        return [
+            await self._get_article_from_db_record(
+                article_row=article_row,
+                slug=article_row[SLUG_ALIAS],
+                author_username=article_row[AUTHOR_USERNAME_ALIAS],
+                requested_user=requested_user,
+            )
+            for article_row in articles_rows
+        ]
