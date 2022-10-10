d0699a9d28e8087715a77689904e231b386f8498
Heechan Kang <weston0713@gmail.com>
2022-10-11 07:56:07 +0900

    221011 article dependency

--- backend/app/api/dependencies/articles.py
+++ backend/app/api/dependencies/articles.py
@@ -1,6 +1,8 @@
 from typing import Optional
+from app.db.errors import EntityDoesNotExist
 
-from fastapi import Query, Path
+from fastapi import Query, Path, Depends, HTTPException
+from starlette import status
 
 from app.models.schemas.articles import (
     DEFAULT_ARTICLES_LIMIT,
@@ -8,6 +10,11 @@ from app.models.schemas.articles import (
     ArticlesFilters,
 )
 from app.models.domain.users import User
+from app.api.dependencies.authentication import get_current_user_authorizer
+from app.api.dependencies.database import get_repository
+from app.db.repositories.articles import ArticlesRepository
+from app.models.domain.articles import Article
+from app.resources import strings
 
 
 def get_articles_filters(
@@ -28,5 +35,13 @@ def get_articles_filters(
 
 async def get_article_by_slug_from_path(
     slug: str = Path(..., min_length=1),
-    user: Optional[User] = Depends()
-)
+    user: Optional[User] = Depends(get_current_user_authorizer(required=False)),
+    articles_repo: ArticlesRepository = Depends(get_repository(ArticlesRepository)),
+) -> Article:
+    try:
+        return await articles_repo.get_article_by_slug(slug=slug, requested_user=user)
+    except EntityDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=strings.USER_IS_NOT_AUTHOR_OF_ARTICLE,
+        )
