de1020768783d3f82eaa7d9e0f386b289f29263d
Heechan Kang <weston0713@gmail.com>
2022-10-10 15:57:37 +0900

    221010 article schema

---
+++ backend/app/api/dependencies/articles.py
@@ -0,0 +1,13 @@
+# from typing import Optional
+
+# from fastapi import Query
+
+# from app.models.schemas.articles import ()
+
+
+# def get_articles_filters(
+#     tag: Optional[str] = None,
+#     author: Optional[str] = None,
+#     favorited: Optional[str] = None,
+#     limit: int = Query(DEFAULT_ARTICLE_LIMIT, ge=1),
+# )
